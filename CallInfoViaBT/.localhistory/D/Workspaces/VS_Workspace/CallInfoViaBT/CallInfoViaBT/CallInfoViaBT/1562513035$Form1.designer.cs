61c8066c05d17a3d80379b32ed963ebde9f1844b
onurpolattimur <onur.polattimur@gmail.com>
2019-07-07 17:31:18 +0300

    Initial commit with this song: https://open.spotify.com/track/0QleSzdcZd8K14NRdEbJZc?si=8mm5NEcsR5uenYwY659bpQ

---
+++ CallInfoViaBT/.localhistory/D/Workspaces/VS_Workspace/CallInfoViaBT/CallInfoViaBT/CallInfoViaBT/1562513035$Form1.designer.cs
@@ -0,0 +1,338 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.callButton = new System.Windows.Forms.Button();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.LWCalls = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.incomingGrpBox = new System.Windows.Forms.GroupBox();
+            this.discoveredDevices = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.findButton = new System.Windows.Forms.Button();
+            this.warningLabel = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.LWOutCalls = new System.Windows.Forms.ListView();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.incomingGrpBox.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(121, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(131, 27);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Phone Number";
+            // 
+            // callButton
+            // 
+            this.callButton.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.callButton.Location = new System.Drawing.Point(121, 61);
+            this.callButton.Name = "callButton";
+            this.callButton.Size = new System.Drawing.Size(131, 33);
+            this.callButton.TabIndex = 2;
+            this.callButton.Text = "Call";
+            this.callButton.UseVisualStyleBackColor = true;
+            this.callButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // connectButton
+            // 
+            this.connectButton.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connectButton.Location = new System.Drawing.Point(543, 59);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(121, 33);
+            this.connectButton.TabIndex = 6;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // LWCalls
+            // 
+            this.LWCalls.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.LWCalls.GridLines = true;
+            this.LWCalls.Location = new System.Drawing.Point(6, 21);
+            this.LWCalls.Name = "LWCalls";
+            this.LWCalls.Size = new System.Drawing.Size(269, 212);
+            this.LWCalls.TabIndex = 6;
+            this.LWCalls.UseCompatibleStateImageBehavior = false;
+            this.LWCalls.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Time";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Phone Number";
+            this.columnHeader2.Width = 94;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Contact Name";
+            this.columnHeader3.Width = 110;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // incomingGrpBox
+            // 
+            this.incomingGrpBox.Controls.Add(this.LWCalls);
+            this.incomingGrpBox.Location = new System.Drawing.Point(12, 163);
+            this.incomingGrpBox.Name = "incomingGrpBox";
+            this.incomingGrpBox.Size = new System.Drawing.Size(281, 239);
+            this.incomingGrpBox.TabIndex = 7;
+            this.incomingGrpBox.TabStop = false;
+            this.incomingGrpBox.Text = "Incoming Calls";
+            // 
+            // discoveredDevices
+            // 
+            this.discoveredDevices.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.discoveredDevices.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.discoveredDevices.FormattingEnabled = true;
+            this.discoveredDevices.Location = new System.Drawing.Point(543, 19);
+            this.discoveredDevices.Name = "discoveredDevices";
+            this.discoveredDevices.Size = new System.Drawing.Size(121, 27);
+            this.discoveredDevices.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(415, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 19);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Select your device";
+            // 
+            // findButton
+            // 
+            this.findButton.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.findButton.Location = new System.Drawing.Point(6, 19);
+            this.findButton.Name = "findButton";
+            this.findButton.Size = new System.Drawing.Size(121, 33);
+            this.findButton.TabIndex = 11;
+            this.findButton.Text = "Find Devices";
+            this.findButton.UseVisualStyleBackColor = true;
+            this.findButton.Click += new System.EventHandler(this.findButton_Click);
+            // 
+            // warningLabel
+            // 
+            this.warningLabel.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.warningLabel.ForeColor = System.Drawing.Color.OrangeRed;
+            this.warningLabel.Location = new System.Drawing.Point(6, 59);
+            this.warningLabel.Name = "warningLabel";
+            this.warningLabel.Size = new System.Drawing.Size(479, 42);
+            this.warningLabel.TabIndex = 12;
+            this.warningLabel.Text = "If you don\'t see your devices in list, please make sure you have paired in window" +
+    "s bluetooth settings.";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.findButton);
+            this.groupBox3.Controls.Add(this.warningLabel);
+            this.groupBox3.Controls.Add(this.discoveredDevices);
+            this.groupBox3.Controls.Add(this.connectButton);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Location = new System.Drawing.Point(12, 22);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(670, 114);
+            this.groupBox3.TabIndex = 13;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Connection";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.callButton);
+            this.groupBox1.Location = new System.Drawing.Point(709, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(275, 114);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Make Call";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.LWOutCalls);
+            this.groupBox2.Location = new System.Drawing.Point(309, 163);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(281, 239);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Outgoing Calls";
+            // 
+            // LWOutCalls
+            // 
+            this.LWOutCalls.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.LWOutCalls.GridLines = true;
+            this.LWOutCalls.Location = new System.Drawing.Point(6, 21);
+            this.LWOutCalls.Name = "LWOutCalls";
+            this.LWOutCalls.Size = new System.Drawing.Size(269, 212);
+            this.LWOutCalls.TabIndex = 6;
+            this.LWOutCalls.UseCompatibleStateImageBehavior = false;
+            this.LWOutCalls.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Time";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Phone Number";
+            this.columnHeader5.Width = 94;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Contact Name";
+            this.columnHeader6.Width = 110;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.WindowText;
+            this.richTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.richTextBox1.Location = new System.Drawing.Point(6, 21);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(368, 212);
+            this.richTextBox1.TabIndex = 15;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.richTextBox1);
+            this.groupBox4.Location = new System.Drawing.Point(597, 163);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(387, 239);
+            this.groupBox4.TabIndex = 16;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Console Output";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(686, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1004, 416);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.incomingGrpBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CallerID via Bluetooth";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.closeConnection);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.incomingGrpBox.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button callButton;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.ListView LWCalls;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.GroupBox incomingGrpBox;
+        private System.Windows.Forms.ComboBox discoveredDevices;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button findButton;
+        private System.Windows.Forms.Label warningLabel;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView LWOutCalls;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
